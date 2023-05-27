c2abb370a8363549db71cef02fd3803ceefb2d6c
srslopes <116354478+srslopes@users.noreply.github.com>
2023-05-27 15:31:02 -0300

    Criação das Classes Aluno e Ficha
    Gets e Sets de ambas as classes
    Métodos para carregar, salvar e excluir objetos do banco de dados
    Conexão com DB pendente

---
+++ Model/Aluno.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitPro.Database;
+
+namespace FitPro
+{
+    internal class Aluno
+    {
+        public int id;
+        private string nome;
+        private int telefone;
+        private DateTime nascimento;
+        private float altura;
+        private Ficha ultima;
+        private List<Ficha> fichas;
+        private Query SQL;
+
+
+        public Aluno()
+        {
+            id = -1;
+            nome = "";
+            nascimento = DateTime.MinValue;
+            fichas = new List<Ficha>();
+            SQL = new Query();
+        }
+
+        public void carregar(int ID)
+        {
+            List<Dictionary<string, object>> query = SQL.Read("aluno");
+        }
+        public void salvar()
+        {
+            if(id<0)
+            {
+               //id = SQL.Insert("aluno");
+            }
+            else
+            {
+                //List<Dictionary<string, object>> query = new List<Dictionary<string, object>>();
+                //SQL.Update("aluno", query , "ID=" +id);
+            }
+        }
+        public void delete()
+        {
+            if (id != -1) SQL.Delete("aluno", "ID="+id);
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setNome(string Nome)
+        {
+            nome = Nome;
+        }
+        public string getNome() 
+        {
+            return nome;
+        }
+
+        public void setTelefone(int Telefone)
+        {
+            telefone = Telefone;
+        }
+        public int getTelefone() 
+        {
+            return telefone;
+        }
+
+        public void setNascimento(DateTime Nascimento)
+        {
+            nascimento = Nascimento;
+        }
+        public DateTime getNascimento()
+        {
+            return nascimento;
+        }
+
+        public void setAltura(float Altura)
+        {
+            altura = Altura;
+        }
+        public float getAltura()
+        {
+            return altura;
+        }
+
+        public void setUltima(Ficha Ultima)
+        {
+            ultima = Ultima;
+        }
+        public Ficha getUltima()
+        {
+            return ultima;
+        }
+
+        public void setFichas(List<Ficha> Fichas)
+        {
+            fichas = Fichas;
+        }
+        public List<Ficha> getFichas()
+        {
+            return fichas;
+        }
+
+    }
+}
---
+++ Model/Ficha.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitPro.Database;
+
+namespace FitPro
+{
+    internal class Ficha
+    {
+        private int id;
+        private DateTime data;
+        private float peso;
+        private float peito;
+        private float cintura;
+        private float bracoL;
+        private float bracoR;
+        private float pernaL;
+        private float pernaR;
+        private string comentarios;
+        private Query SQL;
+
+        public Ficha()
+        {
+            id = -1;
+            data = DateTime.Today;
+            peso = 0;
+            peito = 0;
+            cintura = 0;
+            bracoL = 0;
+            bracoR = 0;
+            pernaL = 0;
+            pernaR = 0;
+            comentarios = "";
+            SQL = new Query();
+        }
+
+        public void carregar(int ID)
+        {
+            List<Dictionary<string, object>> query = SQL.Read("ficha");
+        }
+        public void salvar()
+        {
+            if (id < 0)
+            {
+                //id = SQL.Insert("ficha");
+            }
+            else
+            {
+                //List<Dictionary<string, object>> query = new List<Dictionary<string, object>>();
+                //SQL.Update("ficha", query, "ID=" +id);
+            }
+        }
+        public void delete()
+        {
+            if(id!=-1) SQL.Delete("ficha", "ID=" +id);
+            this.Dispose();
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public DateTime getData()
+        {
+            return data;
+        }
+
+        public void setPeso(float Peso)
+        {
+            peso = Peso;
+        }
+        public float getPeso()
+        {
+            return peso;
+        }
+
+        public void setPeito(float Peito)
+        {
+            peito = Peito;
+        }
+        public float getPeito()
+        {
+            return peito;
+        }
+
+        public void setCintura(float Cintura)
+        {
+            cintura = Cintura;
+        }
+        public float getCintura()
+        {
+            return cintura;
+        }
+
+        public void setBracoL(float BracoL)
+        {
+            bracoL = BracoL;
+        }
+        public float getBracoL()
+        {
+            return bracoL;
+        }
+
+        public void setBracoR(float BracoR)
+        {
+            bracoR = BracoR;
+        }
+        public float getBracoR()
+        {
+            return bracoR;
+        }
+
+        public void setPernaL(float PernaL)
+        {
+            pernaL = PernaL;
+        }
+        public float getPernaL()
+        {
+            return pernaL;
+        }
+
+        public void setPernaR(float PernaR)
+        {
+            pernaR = PernaR;
+        }
+        public float getPernaR()
+        {
+            return pernaR;
+        }
+
+        public void setComentarios(string Comentarios)
+        {
+            comentarios = Comentarios;
+        }
+        public string getComentarios()
+        {
+            return comentarios;
+        }
+
+    }
+}
